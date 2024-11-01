b1a1b778cbff4f0dacb7f43b99b0829c7af7a4c1
Nnatii <Nnatii@DESKTOP-SQB82U9>
2024-11-01 20:30:49 +0100

    Patch103\DBMVCgoodToGoOn

--- src/TNM/Program.cs
+++ src/TNM/Program.cs
@@ -5,10 +5,12 @@ using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<MyApplicationDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(10, 11, 6))
+        new MySqlServerVersion(new Version(10, 11, 6)),
+        b => b.MigrationsAssembly("TNM") // To tak ma byæ bo inaczej migracje sie w Domain robia i nei dzia³a a ja chce modle ³aczenai w Shaerd ale migracje konkretnie dla projketu w konkretnym projekcie
     ));
 
 var app = builder.Build();
